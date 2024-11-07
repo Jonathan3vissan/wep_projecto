3ed0de1f02628cec82e4bf62215f51bfdd790546
Jonnthan3visan <joptrabajo@hotmail.com>
2024-11-07 09:00:43 -0300

    tranferi projecto aca

--- js/RutasHTML.js
+++ js/RutasHTML.js
@@ -7,8 +7,19 @@ class RutasHTML {
             window.location.href = "../html/RegistroUsuario.html";
         });
     }
+/**
+ * redirije a reserva de turno
+ */
+    redirijirReserva() {
+        document.getElementById("reserva").addEventListener("click", () => {
+            window.location.href = "../html/reserva.html"
+        })
+    }
+
+
 }
 
 // Inicializa la clase
 const rutas = new RutasHTML();
 rutas.redirijirARegistro();
+rutas.redirijirReserva();
